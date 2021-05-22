40016c9dc53e01014cd83d53d021933637dd496d
Chulhwan <Chulhwan@DESKTOP-CS3NCGJ>
2021-05-23 00:02:32 +0900

    Project Init

---
+++ GrLandscape/src/main/java/com/gr/land/GrLandscapeApplication.java
@@ -0,0 +1,13 @@
+package com.gr.land;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrLandscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrLandscapeApplication.class, args);
+	}
+
+}
---
+++ GrLandscape/src/test/java/com/gr/land/GrLandscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr.land;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrLandscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
